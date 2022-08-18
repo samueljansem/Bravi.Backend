f6b45a49397f4e453f18357068f53c9604de7649
Samuel Jansem <samuel.jansem@gmail.com>
2022-08-18 16:33:26 -0300

    contact and person json ignore property

--- Domain/Entities/Pessoa.cs
+++ Domain/Entities/Pessoa.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace Bravi.Backend.Domain.Entities;
 
 public class Pessoa
@@ -5,5 +7,7 @@ public class Pessoa
     public Guid Id { get; set; }
     public string Nome { get; set; }
     public DateTime Nascimento { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public IEnumerable<Contato> Contatos { get; set; }
 }
